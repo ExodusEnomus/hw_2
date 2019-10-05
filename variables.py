3354f1fa47abf35b85e23d65e03b6efcdcc4eb44
exodus <den.bur@hotmail.com>
2019-10-05 00:49:43 -0700

    Initial commit

---
+++ variables.py
@@ -0,0 +1,16 @@
+#variables description for stock
+import datetime
+
+ticker = 'AAPL'
+name = 'Apple Inc'
+revenues = 259
+dividends_pay = True
+ex_div_date = datetime.datetime(2019,8,9)
+div_amount = 0.77
+
+print(type(ticker))
+print(type(name))
+print(type(revenues))
+print(type(dividends_pay))
+print(type(ex_div_date))
+print(type(div_amount))
